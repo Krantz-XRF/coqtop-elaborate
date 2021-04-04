25d2969aabe42d94ceb02d37e8f3a859fb1fe253
Xie Ruifeng <Krantz.XRF@outlook.com>
2021-04-04 11:57:59 +0800

    finally! it works perfectly on Basics.v

--- src/CoqTop.hs
+++ src/CoqTop.hs
@@ -8,38 +8,52 @@ import System.Process
 
 import Language.Coq.Parser
 
-breakStatements :: String -> [(Bool, String)]
+breakStatements :: String -> [String]
 breakStatements s = go (zeroPos, s) where
   go = unfoldr \src ->
     if all isSpace (snd src)
       then Nothing
       else Just (breakSentence src)
 
-procCoqtop :: CreateProcess
-procCoqtop = (proc "coqtop" [])
+procCoqtop :: Handle -> CreateProcess
+procCoqtop h = (proc "coqtop" [])
   { std_in = CreatePipe
-  , std_out = CreatePipe
-  , std_err = CreatePipe
+  , std_out = UseHandle h
+  , std_err = UseHandle h
+  , delegate_ctlc = True
   }
-
-while :: IO Bool -> IO a -> IO [a]
-while cond act = do
-  carryOn <- cond
-  if carryOn
-    then (:) <$> act <*> while cond act
-    else pure []
 
 interleave :: [a] -> [a] -> [a]
 interleave []     ys = ys
 interleave (x:xs) ys = x : interleave ys xs
 
+expectWelcome :: Handle -> IO ()
+expectWelcome = void . hGetLine
+
+expectPrompt :: Handle -> IO String
+expectPrompt h = go "" where
+  go xs | " < " `isPrefixOf` xs = pure (reverse xs)
+        | otherwise = do c <- hGetChar h; go (c : xs)
+
+expectPrompt_ :: Handle -> IO ()
+expectPrompt_ = void . expectPrompt
+
+trim :: String -> String
+trim = dropWhile isSpace . reverse . dropWhile isSpace . reverse
+
 coqtop :: String -> IO [Either String String]
-coqtop (breakStatements -> srcLines)
-  = withCreateProcess procCoqtop
-  \ ~(Just hin) ~(Just hout) ~(Just herr) _ -> do
-  _ <- hGetLine hout
-  resLines <- forM srcLines \(ok, ss) -> do
-    hPutStr hin ss; hFlush hin
-    when ok $ void $ hGetLine herr
-    unlines <$> while (hReady hout) (hGetLine hout)
-  pure (interleave (map Right resLines) (map (Left . snd) srcLines))
+coqtop (breakStatements -> srcLines) = do
+  (hout, hout_write) <- createPipe
+  hSetBuffering hout_write NoBuffering
+  hSetNewlineMode hout universalNewlineMode
+  (~(Just hin), ~Nothing, ~Nothing, _)
+    <- createProcess_ "coqtop" (procCoqtop hout_write)
+  expectWelcome hout
+  expectPrompt_ hout
+  resLines <- forM srcLines \ss -> do
+    let ssLine = if last ss == '\n' then ss else ss ++ "\n"
+    let n = length (lines ssLine)
+    hPutStr hin ssLine; hFlush hin
+    prompt <- replicateM n (expectPrompt hout)
+    pure (trim (concatMap (unlines . init . lines) prompt))
+  pure (interleave (map Left srcLines) (map Right resLines))
--- src/Language/Coq/Parser.hs
+++ src/Language/Coq/Parser.hs
@@ -2,7 +2,6 @@ module Language.Coq.Parser where
 
 import Control.Applicative
 import Control.Monad
-import Data.Maybe
 import Language.Coq.Lexer
 
 import Text.Parsec hiding (many, optional, (<|>))
@@ -39,13 +38,13 @@ endOfSentence = char '.' *> peekSat (`elem` " \n\r\t")
 zeroPos :: SourcePos
 zeroPos = initialPos "<INPUT>"
 
-breakSentence :: (SourcePos, String) -> ((Bool, String), (SourcePos, String))
-breakSentence (firstPos, s) = ((ok, this), (finalPos, rest)) where
+breakSentence :: (SourcePos, String) -> (String, (SourcePos, String))
+breakSentence (firstPos, s) = (this, (finalPos, rest)) where
   (this, rest) = splitAt n s
   n = length (takeWhile (<= finalPos) sPos) - 1
   sentencePos
     = setPosition firstPos *> whiteSpace
-    *> ((,) <$> optSentence <*> getPosition)
-  optSentence = isJust <$> optional coqSentence
-  ~(Right (ok, finalPos)) = runParser sentencePos () "<INPUT>" s
+    *> optional coqSentence *> optional newline
+    *> getPosition
+  ~(Right finalPos) = runParser sentencePos () "<INPUT>" s
   sPos = scanl updatePosChar firstPos s
